d5d0d39ddf108324887f243ea3ccba47f98ce670
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-03-12 11:36:10 +0700

    [Fix]: add the target value to the bash script

--- multiple_training.sh
+++ multiple_training.sh
@@ -2,6 +2,7 @@
 
 # :::::::::::::::::: Options ::::::::::::::::::
 MODE="Explain"
+TARGET="Lumen"
 INPUT_SCALERS="Raw MinMax Standard Robust Power"
 OUTPUT_SCALERS="Raw MinMax Standard Robust Power"
 # :::::::::::::::::::::::::::::::::::::::::::::
@@ -10,6 +11,7 @@ for INPUT_SCALER in ${INPUT_SCALERS}; do
   for OUTPUT_SCALER in ${OUTPUT_SCALERS}; do
     python train.py \
     --mode ${MODE} \
+    --target ${TARGET} \
     --feature_scale ${INPUT_SCALER} \
     --target_scale ${OUTPUT_SCALER}
   done
