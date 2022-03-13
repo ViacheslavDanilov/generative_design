34b5a59f889c079004560e16b45fcd8e6cb5af2b
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-03-14 01:45:01 +0700

    [Fix]: scalers are now correctly applied to data

--- tools/model.py
+++ tools/model.py
@@ -11,17 +11,33 @@ class Regressor:
     ):
 
         self.model = AutoML(results_path=model_path)
+
         try:
             self.input_scaler = load(open(os.path.join(model_path, 'input_scaler.pkl'), 'rb'))
-            self.output_scaler = load(open(os.path.join(model_path, 'output_scaler.pkl'), 'rb'))
         except FileNotFoundError:
             self.input_scaler = None
+
+        try:
+            self.output_scaler = load(open(os.path.join(model_path, 'output_scaler.pkl'), 'rb'))
+        except FileNotFoundError:
             self.output_scaler = None
 
-    def __call__(self, data):
+    def __call__(
+            self,
+            data
+    ):
 
-        _data = self.input_scaler.transform(data)
-        output_norm = self.model.predict(_data)
-        output = self.output_scaler.inverse_transform(output_norm.reshape(-1, 1))
+        # TODO: Add support of golden features
+        if self.input_scaler is not None:
+            _data = self.input_scaler.transform(data)
+        else:
+            _data = data.copy()
+
+        _output = self.model.predict(_data)
+
+        if self.output_scaler is not None:
+            output = self.output_scaler.inverse_transform(_output.reshape(-1, 1))
+        else:
+            output = _output.reshape(-1, 1)
 
         return output
