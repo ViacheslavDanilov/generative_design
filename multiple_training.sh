fccd88460ef9c5eb98d6cb8dfd60f123939c344f
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-05-08 18:30:44 +0200

    [Fix]: add multiple targets in the for loop

--- multiple_training.sh
+++ multiple_training.sh
@@ -1,22 +1,24 @@
 #!/usr/bin/env bash
 
-# :::::::::::::::::: Options ::::::::::::::::::
-MODE="Explain"
-TARGET="LMN"
+# :::::::::::::::::::: Options ::::::::::::::::::::
+MODE="Compete"
+TARGETS="Smax LMN"
 INPUT_SCALERS="Raw MinMax Standard Robust Power"
 OUTPUT_SCALERS="Raw MinMax Standard Robust Power"
-METRICS="rmse mse mae mape"
-# :::::::::::::::::::::::::::::::::::::::::::::
+METRICS="mae mse rmse"
+# :::::::::::::::::::::::::::::::::::::::::::::::::
 
-for INPUT_SCALER in ${INPUT_SCALERS}; do
-  for OUTPUT_SCALER in ${OUTPUT_SCALERS}; do
-    for METRIC in ${METRICS}; do
-      python train.py \
-      --mode ${MODE} \
-      --target ${TARGET} \
-      --feature_scale ${INPUT_SCALER} \
-      --target_scale ${OUTPUT_SCALER} \
-      --metric ${METRIC}
+for TARGET in ${TARGETS}; do
+  for INPUT_SCALER in ${INPUT_SCALERS}; do
+    for OUTPUT_SCALER in ${OUTPUT_SCALERS}; do
+      for METRIC in ${METRICS}; do
+        python train.py \
+        --mode ${MODE} \
+        --target ${TARGET} \
+        --feature_scale ${INPUT_SCALER} \
+        --target_scale ${OUTPUT_SCALER} \
+        --metric ${METRIC}
+      done
     done
   done
 done
