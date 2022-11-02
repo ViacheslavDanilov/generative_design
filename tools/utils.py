bcb680cb556d4f1224a14433ba537502a1269f98
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-11-02 11:28:04 +0300

    [Feature]: get list of files with the specified extensions

--- tools/utils.py
+++ tools/utils.py
@@ -1,6 +1,8 @@
+import os
 import re
 import json
-from typing import Tuple
+from pathlib import Path
+from typing import List, Union, Tuple
 
 import numpy as np
 
@@ -53,6 +55,40 @@ def calculate_design_score(
         design_score = 0.0
 
     return lumen_score, stress_score, design_score
+
+
+def get_file_list(
+    src_dirs: Union[List[str], str],
+    ext_list: Union[List[str], str],
+    file_template: str = '',
+) -> List[str]:
+    """
+    Get list of files with the specified extensions
+
+    Args:
+        src_dirs: directory(s) with files inside
+        ext_list: extension(s) used for a search
+        file_template: include files with this template
+    Returns:
+        all_files: a list of file paths
+    """
+
+    all_files = []
+    src_dirs = [src_dirs, ] if isinstance(src_dirs, str) else src_dirs
+    ext_list = [ext_list, ] if isinstance(ext_list, str) else ext_list
+    for src_dir in src_dirs:
+        for root, dirs, files in os.walk(src_dir):
+            for file in files:
+                file_ext = Path(file).suffix
+                file_ext = file_ext.lower()
+                if (
+                    file_ext in ext_list
+                    and file_template in file
+                ):
+                    file_path = os.path.join(root, file)
+                    all_files.append(file_path)
+    all_files.sort()
+    return all_files
 
 
 def get_golden_features(
