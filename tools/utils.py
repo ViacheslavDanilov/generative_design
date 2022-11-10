4e305a083f7b17a21a23dedaffdfc0d7b7a4c1e0
Viacheslav Danilov <viacheslav.v.danilov@gmail.com>
2022-11-10 12:42:18 +0300

    Rework tuning pipeline (#12)
    * [Feature]: add several features to the tuning pipeline
    
    * Add computation of feature importance values
    
    * Compute and save best scores during tuning
    
    * [Feature]: implement design_score similar to the F-beta measure
    
    * [Feature]: add lumen_score and stress_score to the output data frame
    
    * [Feature]: implement F-beta-based optimization
    
    * [Feature]: get a list of files with the specified extensions
    
    * [Fix]: exclude temporal files and scripts
    
    * [Bugfix]: fix the selection of normalization methods

--- tools/utils.py
+++ tools/utils.py
@@ -1,5 +1,8 @@
+import os
 import re
 import json
+from pathlib import Path
+from typing import List, Union, Tuple
 
 import numpy as np
 
@@ -24,7 +27,8 @@ def calculate_design_score(
         lumen_abs: float,
         stress_abs: float,
         uts: float,
-) -> float:
+        alpha: float = 1.0,
+) -> Tuple[float, float, float]:
 
     if (
             not np.isnan(lumen_abs)
@@ -40,12 +44,51 @@ def calculate_design_score(
             uts=uts,
         )
 
-        design_score = np.sqrt(lumen_score * stress_score)
+        # alpha is chosen such that lumen_score is considered alpha times as important as stress_score
+        design_score_numer = (1 + alpha**2) * stress_score * lumen_score
+        design_score_denom = alpha**2 * stress_score + lumen_score
+        design_score = design_score_numer / design_score_denom
 
     else:
-        design_score = 0
+        lumen_score = 0.0
+        stress_score = 0.0
+        design_score = 0.0
 
-    return design_score
+    return lumen_score, stress_score, design_score
+
+
+def get_file_list(
+    src_dirs: Union[List[str], str],
+    ext_list: Union[List[str], str],
+    file_template: str = '',
+) -> List[str]:
+    """
+    Get list of files with the specified extensions
+
+    Args:
+        src_dirs: directory(s) with files inside
+        ext_list: extension(s) used for a search
+        file_template: include files with this template
+    Returns:
+        all_files: a list of file paths
+    """
+
+    all_files = []
+    src_dirs = [src_dirs, ] if isinstance(src_dirs, str) else src_dirs
+    ext_list = [ext_list, ] if isinstance(ext_list, str) else ext_list
+    for src_dir in src_dirs:
+        for root, dirs, files in os.walk(src_dir):
+            for file in files:
+                file_ext = Path(file).suffix
+                file_ext = file_ext.lower()
+                if (
+                    file_ext in ext_list
+                    and file_template in file
+                ):
+                    file_path = os.path.join(root, file)
+                    all_files.append(file_path)
+    all_files.sort()
+    return all_files
 
 
 def get_golden_features(
